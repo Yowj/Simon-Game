0b89d01e34c2f39f4f7daa719891dd8d6ef449da
Yowj <153867298+Yowj@users.noreply.github.com>
2023-12-15 05:06:41 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,88 @@
+
+var buttonColours =["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var playerPattern = [];
+var started = false;
+var level = 0;
+
+
+//////start
+
+$(document).keypress(function() {
+    if (!started) {
+      nextSequence();
+      started = true;
+    }
+  });  
+/////
+
+    
+
+$(".btn").click(function(){
+ var userChosenColour = $(this).attr("id");
+ playerPattern.push(userChosenColour);
+playSound(userChosenColour);
+makeAnimation(userChosenColour);
+check();
+})
+
+
+////////////////////////////////////////////////////////// labas ng game
+
+function check(){
+if (gamePattern.length===playerPattern.length && gamePattern[level - 1]===playerPattern[level - 1]){
+    setTimeout(function () {
+        nextSequence();
+    }, 500);  
+ }
+
+else if (gamePattern[playerPattern.length-1]!==playerPattern[playerPattern.length-1]){
+    wrong();
+    started = false;
+    level = 0;
+
+}
+}
+
+//playsounds
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+   
+  
+  //make animation haha
+function makeAnimation(name){
+    $("#" + name).fadeIn(100).fadeOut(100).fadeIn(100);   
+}
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4); 
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    setTimeout(function () {
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); 
+    }, 1000);
+
+    level++;
+    $("#level-title").text("Umpisa na ang laban, nasa level " + level + " ka!");
+    playerPattern.length = 0;
+}
+
+function wrong(){
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("red");
+
+    setTimeout(function(){
+        $("body").removeClass("red"); 
+    }, 200)
+    $("#level-title").text("Press any key to restart");
+    var started = false;
+    var level = 0;
+    playerPattern.length = 0;
+    gamePattern.length = 0;
+
+}
+
